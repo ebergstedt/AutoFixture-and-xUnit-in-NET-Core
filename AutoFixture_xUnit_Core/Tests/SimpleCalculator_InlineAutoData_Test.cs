157fa8e03040df7754534c0c89adbb40932e5940
DESKTOP-3I35F7B\bergs <erik.bergstedt@collectorbank.se>
2018-02-18 13:09:26 +0100

    inline and auto data

--- AutoFixture_xUnit_Core/Tests/SimpleCalculator_InlineAutoData_Test.cs
+++ AutoFixture_xUnit_Core/Tests/SimpleCalculator_InlineAutoData_Test.cs
@@ -6,30 +6,30 @@ using Xunit;
 
 namespace AutoFixture_xUnit_Core.Tests
 {
-    public class SimpleCalculator_AutoData_Test
+    public class SimpleCalculator_InlineAutoData_Test
     {
         [Theory]        
-        [AutoData]
-        public void SimpleCalculator_AutoData_Addition(int a, int b)
+        [InlineAutoData(2, 1)]
+        public void SimpleCalculator_InlineAutoData_Addition(int a, int b)
         {
             var calc = new SimpleCalculator();
 
             calc.Add(a);
             calc.Add(b);
 
-            Assert.True(calc.Result == a + b);
+            Assert.True(calc.Result == 3);
         }
 
         [Theory]
-        [AutoData]
-        public void SimpleCalculator_AutoData_Subtraction(int a, int b)
+        [InlineAutoData(2, 1)]
+        public void SimpleCalculator_InlineAutoData_Subtraction(int a, int b)
         {
             var calc = new SimpleCalculator();
 
             calc.Subtract(a);
             calc.Subtract(b);
 
-            Assert.True(calc.Result == 0 - a - b);
+            Assert.True(calc.Result == -3);
         }
     }
 }
